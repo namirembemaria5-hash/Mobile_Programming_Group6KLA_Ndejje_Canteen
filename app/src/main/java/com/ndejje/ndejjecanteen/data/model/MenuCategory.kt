2dc1cd78ba2095c85a66520e4533c1a7601bff2f
RUSTEN <wangadyarusten@gmail.com>
2026-04-14 10:46:31 +0300

    created menu category.kt

---
+++ app/src/main/java/com/ndejje/ndejjecanteen/data/model/MenuCategory.kt
@@ -0,0 +1,9 @@
+package com.ndejje.canteen.data.model
+
+enum class MenuCategory(val displayName: String) {
+    SNACKS("Snacks"),
+    DRINKS("Drinks"),
+    TEA_COFFEE("Tea & Coffee"),
+    BUFFET("Buffet"),
+    SPECIAL_ORDERS("Special Orders")
+}
